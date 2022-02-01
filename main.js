27dcf7a5507e27e93739002be5b55b709047a999
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2022-02-02 00:54:14 +0900

    正規表現が使えるようになったが、リスト生成に問題がある

--- main.js
+++ main.js
@@ -4,22 +4,25 @@ function mainProc() {
    -----------------------*/
   // 検索文字列の取得
   const searchStr = document.querySelector("#search").value;
+  const searchRegExp = new RegExp(searchStr, "");
   // ポケモンリストをロード
   const pokemons = loadPokemon();
   // 検索
-  const searched = pokemons.find((p, i) => p === searchStr);
+  const searched = pokemons.filter((p, i) => searchRegExp.test(p));
 
   /*-----------------------
       検索結果の追加
    -----------------------*/
   // 結果のクリア
-  const resultArea = document.querySelector("#resultArea");
+  const resultArea = document.querySelector("#resultList");
   resultArea.childNodes.forEach((e) => e.remove());
-  // 結果エレメントの生成
-  const result = document.createElement("span");
-  result.textContent = searched;
-  // 結果の追加
-  resultArea.appendChild(result);
+  for (s of searched) {
+      // 結果エレメントの生成
+      const result = document.createElement("li");
+      result.textContent = s;
+      // 結果の追加
+    resultArea.appendChild(result);
+  }
 }
 
 window.onload = () => {
