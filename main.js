a8c2b9165007f4590b2041582fabcdf242368c43
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2022-02-02 00:29:48 +0900

    リファクタ

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-function mainProc(){
-    const searchStr = document.querySelector('#search').value
-    const pokemons = loadPokemon();
+function mainProc() {
+  /*-----------------------
+     検索実行
+   -----------------------*/
+  // 検索文字列の取得
+  const searchStr = document.querySelector("#search").value;
+  // ポケモンリストをロード
+  const pokemons = loadPokemon();
+  // 検索
+  const searched = pokemons.find((p, i) => p === searchStr);
 
-    const searched = pokemons.find((p,i)=>p===searchStr)
-
-    const result = document.createElement('span')
-    result.textContent = searched
-
-    const resultArea = document.querySelector('#resultArea')
-    resultArea.childNodes.forEach((e)=>e.remove())
-    resultArea.appendChild(result)
+  /*-----------------------
+      検索結果の追加
+   -----------------------*/
+  // 結果のクリア
+  const resultArea = document.querySelector("#resultArea");
+  resultArea.childNodes.forEach((e) => e.remove());
+  // 結果エレメントの生成
+  const result = document.createElement("span");
+  result.textContent = searched;
+  // 結果の追加
+  resultArea.appendChild(result);
 }
 
-
-window.onload = ()=>{
-    document.querySelector('#go').addEventListener('click', ()=>{
-        mainProc()
-    })
-}
+window.onload = () => {
+  document.querySelector("#go").addEventListener("click", () => {
+    mainProc();
+  });
+};
