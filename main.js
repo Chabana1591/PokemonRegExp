89f0ab31903de3979e9114d2ce345376cba2b6d0
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2022-02-02 00:12:55 +0900

    完全一致でとりあえず実装

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function mainProc(){
-    alert("ok");
+    const searchStr = document.querySelector('#search').value
+    const pokemons = loadPokemon();
+
+    const searched = pokemons.find((p,i)=>p===searchStr)
+
+    const result = document.createElement('span')
+    result.textContent = searched
+
+    document.querySelector('#resultArea').appendChild(result)
 }
 
 
